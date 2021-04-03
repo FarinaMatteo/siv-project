939b7c9089df4cfd50a6de6a4bf6e96045b071cf
FarinaMatteo <matteo.farina-1@studenti.unitn.it>
2021-04-03 14:24:36 +0200

    optical flow method

---
+++ hsv_optflow.py
@@ -0,0 +1,194 @@
+"""
+Background vs Foreground Image segmentation. The goal is to produce a segmentation map that imitates
+videocalls tools like the ones implemented in Google Meet, Zoom without using Deep Learning- or Machine Learning-
+based techniques.  
+   
+This script does the following:  
+- builds a background model using approximately the first 3s of the video, acting on the HSV colorspace;  
+- performs frame differencing in the HSV domain;  
+- runs LP filtering (median-filter) on the Saturation difference;  
+- uses Otsu's technique to threshold the saturation and the brightness difference;  
+- concatenates the saturation and the brightness masks to produce the foreground mask;  
+- uses the foreground mask, the current video stream and a pre-defined background picture to produce the final output.  
+
+Additionally, the script provides methods to reset the background (by pressing the 'r' key) and to save the current mask
+(by pressing the 's' key). In the latter: 
+- Lucas-Kanade optical flow algorithm is applied in order to continuously track the foreground points;
+- Median LP filtering is performed to denoise the optical flow output;   
+- the convex hull algorithm is applied to build a shape around the tracked points;   
+- the resulting mask is then used with logical bitwise operators to separate foreground and background.   
+   
+Authors: M. Farina, F. Diprima - University of Trento
+Last Update (dd/mm/yyyy): 03/04/2021 
+"""
+
+import cv2
+import time
+import numpy as np
+
+# initialize video capture
+cap = cv2.VideoCapture(0)
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+ms = int(1000/fps)
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# initialize video writer
+codec = cv2.VideoWriter_fourcc(*'mp4v')
+writer = cv2.VideoWriter('report_videos/optflow.mp4', codec, fps, frameSize=(width, height))
+
+# define the variables for image-processing tasks
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(3,5))
+n_rows = 480
+n_cols = 720
+dst_size = (n_cols, n_rows)
+dst_shape = (n_rows, n_cols, 3)
+bg_frame_limit = fps * 3  # number of frames in 3 seconds
+gauss_kernel = (15,15)
+median_ksize = 11
+
+# setup an image to replace the background
+bg_pic_path = "/home/teofa/Pictures/Background/catalina-day.jpg"
+bg_pic = cv2.imread(bg_pic_path)
+bg_pic = cv2.resize(bg_pic, dst_size)
+
+
+def run():
+    """
+        Main loop for background removal. Uses frame differencing together with
+        morphological operators to separate background from foreground for videoconferencing.
+    """
+    time_lst = [0]
+    
+    # initialize background
+    hsv_bg = np.zeros(dst_shape, dtype='uint16')
+    black_bg = np.zeros(dst_shape[:-1], dtype='uint8')
+
+    # initialize vector of points for opticalFlow
+    start_points = np.array([], dtype=np.float32)
+    mask_saved = False
+    prev_gray = None
+
+    # start looping through frames
+    frame_count = 0
+    if cap.isOpened():
+        while cap.isOpened():
+            # retrieve the current frame and exit if needed
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            # otherwise, perform basic operations on the current frame
+            frame = cv2.resize(frame, dst_size)
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray_frame = cv2.GaussianBlur(gray_frame, ksize=gauss_kernel, sigmaX=2, sigmaY=2)
+            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            hsv_frame_blurred = cv2.GaussianBlur(hsv_frame, gauss_kernel, sigmaX=2, sigmaY=2)
+            
+            # build a model for the background during the first frames
+            if frame_count < bg_frame_limit:
+                hsv_bg = hsv_bg.copy() + hsv_frame_blurred
+                if frame_count == bg_frame_limit-1:
+                    hsv_bg = np.uint8(hsv_bg.copy() / bg_frame_limit)
+
+            # when the bg has been modeled, segment the fg
+            else:
+                time_in = time.perf_counter()
+                if mask_saved:
+                    if len(start_points) == 0:
+                        indices = np.where(fg_mask_closed == 255)
+                        start_points = np.array([[round(indices[1][i]), round(indices[0][i])] for i in range(len(indices[0]))], dtype=np.float32)
+                        points = start_points
+                        status = np.array([[1]]*len(points))
+                    else:
+                        prev_points = np.array(points, dtype=np.float32)
+                        points, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, gray_frame, prev_points, None, winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+                    
+                    point_set = np.array(points, dtype=np.int32)
+                    point_set_ = point_set.copy()[status[:,0]==1]
+                    rows = point_set_[:, 1]
+                    max_row_shift = max(rows) - n_rows + 1 if max(rows) >= n_rows else 1 
+                    cols = point_set_[:, 0]
+                    max_col_shift = max(cols) - n_cols + 1 if max(cols) >= n_cols else 1
+                    rows = rows - max_row_shift
+                    cols = cols - max_col_shift
+                    fg_mask_closed = black_bg.copy()
+                    fg_mask_closed[rows, cols] = 255
+                    fg_mask_closed = cv2.medianBlur(fg_mask_closed.copy(), ksize=15)
+                    point_set_median = np.where(fg_mask_closed == 255)
+                    point_set_median = np.array([[round(point_set_median[1][i]), round(point_set_median[0][i])] for i in range(len(point_set_median[0]))], dtype=np.float32)                    
+                    if len(point_set_median) > 0:
+                        hull = np.array(cv2.convexHull(point_set_median), dtype=np.int32)
+                        fg_mask_closed = cv2.fillConvexPoly(black_bg.copy(), hull, 255)
+                    
+                else:
+                    # perform frame differencing
+                    diff = cv2.absdiff(hsv_frame_blurred, hsv_bg)
+                    h_diff, s_diff, v_diff = cv2.split(diff)
+                    
+                    # automatic global thresholding with Otsu's technique
+                    r1, h_diff_thresh = cv2.threshold(h_diff, 1, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                    r2, s_diff_thresh = cv2.threshold(s_diff, 1, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                    r3, v_diff_thresh = cv2.threshold(v_diff, 1, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                    
+                    # take into account contribution of saturation and value (aka 'brightness')
+                    # clean the saturation mask beforehand, it usually is more unstable
+                    s_diff_thresh_median = cv2.medianBlur(s_diff_thresh, ksize=median_ksize)
+                    fg_mask = s_diff_thresh_median + v_diff_thresh
+                    fg_mask_closed = cv2.morphologyEx(fg_mask, cv2.MORPH_CLOSE, kernel=kernel, iterations=10)
+                    fg_mask_closed = cv2.dilate(fg_mask_closed.copy(), kernel)
+                    
+                # compute the actual foreground and background
+                foreground = cv2.bitwise_and(frame, frame, mask=fg_mask_closed)
+                background = bg_pic - cv2.bitwise_and(bg_pic, bg_pic, mask=fg_mask_closed)
+                
+                # ... and add them to generate the output image
+                output_weighted = cv2.addWeighted(foreground, 0.8, bg_pic, 0.2, 0)
+                out = cv2.add(foreground, background)
+                
+                # display the output and the masks
+                cv2.imshow("Background vs Foreground", cv2.hconcat([background, foreground]))
+                cv2.imshow("Output - Weighted vs Original", cv2.hconcat([output_weighted, out]))
+                try:
+                    cv2.imshow("Optical Flow Mask", fg_mask_closed)
+                except Exception as e:
+                    print("Not showing opt flow mask for this reason: ", e)
+            
+                # quit if needed
+                key = cv2.waitKey(ms)
+                if key==ord('q'):
+                    break
+
+                # if user presses 's' save and track the current mask with optical flow assumptions
+                elif key == ord('s'):
+                    mask_saved = True
+                       
+                # if user presses 'r', reset the background model:
+                elif key == ord('r'):
+                    mask_saved = False
+                    start_points = np.array([], dtype=np.float32)
+                    frame_count = -1
+                    hsv_bg = np.zeros(dst_shape, dtype='uint16')
+                
+
+                if writer:
+                    writer.write(cv2.resize(out, dsize=(width, height)))
+                
+
+                # keep track of time
+                time_out = time.perf_counter()
+                time_diff = time_out - time_in
+                time_lst.append(time_diff)
+
+            prev_gray = gray_frame.copy()
+            frame_count += 1
+
+    print("Average Time x Frame: ", round(np.sum(np.array(time_lst))/len(time_lst), 2))
+    cv2.destroyAllWindows()
+    cap.release()
+    if writer:
+        writer.release()
+
+if __name__ == "__main__":
+    run()
